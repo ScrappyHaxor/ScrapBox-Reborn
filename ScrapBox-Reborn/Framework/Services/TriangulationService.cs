efb3c42976abba382f17c057baa5cf0c85f67448
Hampus <hampus.hansen@hotmail.com>
2022-04-26 10:22:32 +0200

    Fix for first commit

--- ScrapBox-Reborn/Framework/Services/TriangulationService.cs
+++ ScrapBox-Reborn/Framework/Services/TriangulationService.cs
@@ -13,40 +13,43 @@ namespace ScrapBox.Framework.Services
 
     public class TriangulationService
     {
-        public void Triangulate(ScrapVector[] verts, TriangulationMethod method)
+        public ScrapVector[] Triangulate(ScrapVector[] verts, TriangulationMethod method)
         {
             switch (method)
             {
                 case TriangulationMethod.EAR_CLIPPING:
-                    EarClipping(verts);
-                    break;
+                    return EarClipping(verts);
             }
+
+            return default;
         }
 
-        public void EarClipping(ScrapVector[] verts)
+        public ScrapVector[] EarClipping(ScrapVector[] verts)
         {
-            List<ScrapVector> trianglePositions;
-
             if (verts == null)
             {
                 LogService.Log("TriangulationService", "EarClipping", "Array of verticies is null.", Severity.ERROR);
-                return;
+                return verts;
             }
 
             if (verts.Length < 3)
             {
                 LogService.Log("TriangulationService", "EarClipping", "Less than 3 verticies in array.", Severity.ERROR);
+                return verts;
             }
+
+            int triangleCount = verts.Length - 2;
+            int totalTriangleIndexCount = triangleCount * 3;
+
+
 
             List<ScrapVector> workingSet = new List<ScrapVector>(verts);
-            for (int i = 0; i < verts.Length; i++)
+            while (workingSet.Count > 3)
             {
-                ScrapVector p1 = Standard.FetchIndex(i - 1, verts);
-                ScrapVector p2 = Standard.FetchIndex(i, verts);
-                ScrapVector p3 = Standard.FetchIndex(i + 1, verts);
-
-
+                
             }
+
+            return default;
         }
 
     }
