966b9b7c6cd1767d82a4704fb6cf7c228e7419ae
Kobbe98 <mansowl@outlook.com>
2022-05-22 18:12:03 +0200

    Updated rendering
    The bar now updates when to space bar is held.

--- ScrapBox-Reborn/Framework/ECS/Components/ProgressBar.cs
+++ ScrapBox-Reborn/Framework/ECS/Components/ProgressBar.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ScrapBox.Framework.Level;
+using ScrapBox.Framework.Math;
 using ScrapBox.Framework.Services;
 using System;
 using System.Collections.Generic;
@@ -20,17 +21,26 @@ namespace ScrapBox.Framework.ECS.Components
 
         public Rectangle Foreground;
         public Rectangle Background;
-        public int MaxValue = 0;
-        public int MinValue = 0;
 
-        public ProgressBar()
+        public int MaxValue { get; private set; }
+        public int MinValue { get; private set; }
+        public double CurrentValue { get; private set; }
+
+        public ProgressBar(int maxValue, int minValue = 0)
         {
 
         }
 
-        public void SetValue()
+        public void SetValue(double value)
         {
+            CurrentValue += value;
+        }
 
+        public void Reset()
+        {
+            //it is set to 2 due to an error with the transform
+            CurrentValue = 2;
+            Foreground = new Rectangle(Transform.Position, new ScrapVector(CurrentValue, Transform.Dimensions.Y));
         }
 
         public override void Awake()
@@ -44,30 +54,44 @@ namespace ScrapBox.Framework.ECS.Components
 
             if(BackgroundColor == default)
             {
-                BackgroundColor = Color.DarkGray;
+                BackgroundColor = Color.Blue;
             }
 
             if(MaxValue == 0)
             {
-                MaxValue = 100;
+                MaxValue = 300;
             }
 
-            Foreground = new Rectangle(Transform.Position, Transform.Dimensions);
-            Background = new Rectangle(Transform.Position, Transform.Dimensions);
+            Foreground = new Rectangle(Transform.Position, new ScrapVector(CurrentValue, Transform.Dimensions.Y));
+            Background = new Rectangle(Transform.Position, new ScrapVector(MaxValue, Transform.Dimensions.Y));
         }
 
         internal override void Tick()
         {
+            Foreground.Dimensions = new ScrapVector(CurrentValue, Transform.Dimensions.Y);
+
             base.Tick();
         }
 
         internal override void Render(Camera mainCamera)
         {
-            TriangulationService.Triangulate(Foreground.Verticies, TriangulationMethod.EAR_CLIPPING, out int[] foreIndicies);
-            Renderer.RenderPolygon(Foreground.Verticies, foreIndicies, ForegroundColor, mainCamera);
+            //  temporary workaround due to errors caused by transform or renderer.
+            //  the rectangle is not drawn if the X-value of its transform is set to 0.
+            //  the program gets stuck in a loop if the X-value is set to 1.
+            if(CurrentValue < 3)
+            {
+                ForegroundColor = Color.Transparent;
+            }
+            else
+            {
+                ForegroundColor = Color.Green;
+            }
 
             TriangulationService.Triangulate(Background.Verticies, TriangulationMethod.EAR_CLIPPING, out int[] backIndicies);
-            Renderer.RenderPolygon(Foreground.Verticies, backIndicies, BackgroundColor, mainCamera);
+            Renderer.RenderPolygon(Background.Verticies, backIndicies, BackgroundColor, mainCamera);
+
+            TriangulationService.Triangulate(Foreground.Verticies, TriangulationMethod.EAR_CLIPPING, out int[] foreIndicies);
+            Renderer.RenderPolygon(Foreground.Verticies, foreIndicies, ForegroundColor, mainCamera);
 
             base.Render(mainCamera);
         }
